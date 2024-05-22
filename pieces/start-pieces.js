c48075ac9fcfc99a368e04cf77f2cc4f6202aa53
wreeshab <evrishab@gmail.com>
2024-05-22 10:57:00 +0530

    file management

---
+++ pieces/start-pieces.js
@@ -0,0 +1,35 @@
+
+let startPieces = ["", "", "",topCannon, "", topTitan, "",  "",
+topTank ,topSemiRicochet,"" ,topRicochet, "", "", "",  "",
+"", "", "", "", "", "", "", "",
+"", "", "", "", "", "", "", "",
+"", "", "", "", "", "", "", "",
+"", "", bottomRicochet, "", "", bottomSemiRicochet, "", "",
+"","", "","", "", "", "","",
+bottomTank, "", "",bottomCannon, "",  "", "", bottomTitan
+];
+
+const initialPieces = ["", "", "", "", topTitan, "", topCannon, "",
+"", "", topTank,topSemiRicochet, topRicochet, "", "",  "",
+"", "", "", "", "", "", "", "",
+"", "", "", "", "", "", "", "",
+"", "", "", "", "", "", "", "",
+"", "", "", "", "", "", "", "",
+"",bottomRicochet, "", "", "",  bottomSemiRicochet, "", "",
+bottomTank, "", "", "", bottomCannon, "", "", bottomTitan
+];
+
+const bluePieces = [
+    topCannon,
+    topRicochet,
+    topSemiRicochet,
+    topTitan,
+    topTank,
+  ];
+  const greenPieces = [
+    bottomCannon,
+    bottomRicochet,
+    bottomSemiRicochet,
+    bottomTank,
+    bottomTitan,
+  ];  
