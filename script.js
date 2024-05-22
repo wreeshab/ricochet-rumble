9e3a0599e6c61a81ea09b7eb0aeb6adb4456dc1a
wreeshab <evrishab@gmail.com>
2024-05-22 14:32:32 +0530

    add home page

---
+++ script.js
@@ -0,0 +1,18 @@
+const rainContainer = document.querySelector('.rain-container');
+
+function createRaindrop() {
+    const raindrop = document.createElement('div');
+    raindrop.classList.add('raindrop');
+    raindrop.style.left = `${Math.random() * 100}vw`;
+    raindrop.style.animationDuration = `${Math.random() * 0.5 + 3}s`;
+    raindrop.style.opacity = `${Math.random()}`;
+    raindrop.style.height = `${Math.random() * 7 + 3}vh`;
+    rainContainer.appendChild(raindrop);
+
+    // Remove raindrop after animation is complete to prevent too many elements in the DOM
+    setTimeout(() => {
+        raindrop.remove();
+    }, 10000);
+}
+
+setInterval(createRaindrop,30);
