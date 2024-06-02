f13034f4473f9e3ed3698955cf9dbef3180c54b0
wreeshab <evrishab@gmail.com>
2024-06-02 20:58:38 +0530

    commit for pages

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function createRaindrop() {
   const raindrop = document.createElement("div");
   raindrop.classList.add("raindrop");
   raindrop.style.left = `${Math.random() * 100}vw`;
-  raindrop.style.animationDuration = `${Math.random() * 0.5 + 10}s`;
+  raindrop.style.animationDuration = `${Math.random() * 0.5 + 13}s`;
   raindrop.style.opacity = `${Math.random()}`;
-  raindrop.style.height = `${Math.random() * 7 + 20}vh`;
+  raindrop.style.height = `${Math.random() * 7 + 23}vh`;
   rainContainer.appendChild(raindrop);
 
   // Remove raindrop after animation is complete to prevent too many elements in the DOM
@@ -15,4 +15,4 @@ function createRaindrop() {
   }, 40000);
 }
 
-setInterval(createRaindrop, 400);
+setInterval(createRaindrop, 500);
